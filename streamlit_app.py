771b9db4e74184a6fa337b5a0458d234cb15dd55
Jiranya-Cee07 <09jiranya@gmail.com>
2026-08-05 19:58:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,4 @@ st.info('ข้อมูลสำหรับทำนาย')
 
 st.subheader("ข้อมูลทั้งหมด")
 dt = pd.read_csv("./data/Heart3.csv")
-st.dataframe(dt, use_container_width=True, height=400)
+st.dataframe(dt, use_container_width=True, height=300)
