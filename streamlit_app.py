6770b2da94e84442b565909304f3a8ebda0d7d4d
Jiranya-Cee07 <09jiranya@gmail.com>
2026-08-05 19:50:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
-st.title('🤖 Machine learning app')
+st.title('Heart Disease Predictions')
 
-st.info('This is app bbuilds a machine learning model!')
+st.info('ข้อมูลสำหรับทำนาย')
+
+st.subheader("ข้อมูลทั้งหมด")
+dt = pd.read_csv("./data/Heart3.csv")
+st.dataframe(dt, use_container_width=True, height=500)
