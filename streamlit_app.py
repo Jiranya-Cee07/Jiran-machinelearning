221956d7fd3868344db93b88a17437f601799639
Jiranya-Cee07 <09jiranya@gmail.com>
2026-08-05 20:26:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,8 @@ st.title('Heart Disease Predictions')
 
 st.info('ข้อมูลสำหรับทำนาย')
 
-with st.expander('ข้อมูลทั้งหมด'):
+with st.expander('ข้อมูล Heart Disease Predictions'):
+  st.white('ข้อมูลทั้งหมด')
   dt = pd.read_csv("./data/heart.csv")
   st.dataframe(dt, use_container_width=True, height=300)
 
