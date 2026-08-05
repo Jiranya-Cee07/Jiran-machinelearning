00515af0e147bf40d0855e354106a5d6badc935e
Jiranya-Cee07 <09jiranya@gmail.com>
2026-08-05 20:24:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,5 @@ st.info('ข้อมูลสำหรับทำนาย')
 
 with st.expander('ข้อมูลทั้งหมด'):
   dt = pd.read_csv("./data/heart.csv")
-  print('Shape of the data is ', data.shape)
   st.dataframe(dt, use_container_width=True, height=300)
 
