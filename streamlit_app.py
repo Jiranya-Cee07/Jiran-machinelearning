3055afde2b17c556a405c5425eda09ade0d4347f
Jiranya-Cee07 <09jiranya@gmail.com>
2026-08-05 20:22:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.title('Heart Disease Predictions')
 st.info('ข้อมูลสำหรับทำนาย')
 
 with st.expander('ข้อมูลทั้งหมด'):
-  data = pd.read_csv('/kaggle/input/heart-disease/heart.csv')
+  dt = pd.read_csv("./data/Heart.csv")
   print('Shape of the data is ', data.shape)
   st.dataframe(dt, use_container_width=True, height=300)
 
