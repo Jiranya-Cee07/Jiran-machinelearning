16fce4867bc723135faafeb9f9154e12af6ee770
Jiranya-Cee07 <09jiranya@gmail.com>
2026-08-07 16:30:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -95,6 +95,10 @@ with tab2:
 st.subheader("📈 สถิติพื้นฐานของข้อมูล")
 st.write(dt.describe(include="all"))
 
+# การเลือกแสดงกราฟตามฟีเจอร์
+st.subheader("📌 เลือกฟีเจอร์เพื่อดูการกระจายข้อมูล")
+feature = st.selectbox("เลือกฟีเจอร์", dt.columns[:-1])
+
 with st.sidebar:
     st.header('รับข้อมูล')
     Age = st.slider('อายุ(Age)', 1, 100, 20)
